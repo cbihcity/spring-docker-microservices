739461972ab535ae4e0e6543be1a72567a97dab1
ihhe0417 <xanderakk@mail.ru>
2017-05-23 13:49:54 +0300

    add bootstrap.test.config

--- spring-docker-app/src/test/java/com/apress/spring/SimpleJpaAppApplicationTests.java
+++ spring-docker-app/src/test/java/com/apress/spring/SimpleJpaAppApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,7 +23,6 @@ import java.text.ParseException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(locations = "classpath:test.properties")
 public class SimpleJpaAppApplicationTests {
 
     @Autowired
