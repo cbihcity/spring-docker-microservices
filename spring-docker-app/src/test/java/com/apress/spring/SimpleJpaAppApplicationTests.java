8116c65aa32cc448548a1ce55474ffa571687b56
ihhe0417 <xanderakk@mail.ru>
2017-05-24 15:32:56 +0300

    commit

--- spring-docker-app/src/test/java/com/apress/spring/SimpleJpaAppApplicationTests.java
+++ spring-docker-app/src/test/java/com/apress/spring/SimpleJpaAppApplicationTests.java
@@ -12,5 +12,4 @@ public class SimpleJpaAppApplicationTests {
     @Test
     public void contextLoads() {
     }
-
 }
--- spring-mongo-app/src/test/java/com/apress/spring/SimpleMongoAppApplicationTests.java
+++ spring-mongo-app/src/test/java/com/apress/spring/SimpleMongoAppApplicationTests.java
@@ -2,15 +2,14 @@ package com.apress.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SimpleMongoAppApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SimpleMongoAppApplicationTests {
 
     @Test
     public void contextLoads() {
     }
-
 }
