1391c5e9804b3564c077628b1d55ade216499081
ihhe0417 <xanderakk@mail.ru>
2017-05-24 13:41:02 +0300

    Revert "commit"
    This reverts commit 8d4c5122952dfdf24609a2e787267b62fede2eee.

--- spring-mongo-app/src/test/java/com/apress/spring/SimpleMongoAppApplicationTests.java
+++ spring-mongo-app/src/test/java/com/apress/spring/SimpleMongoAppApplicationTests.java
@@ -2,14 +2,15 @@ package com.apress.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SimpleMongoAppApplication.class)
 public class SimpleMongoAppApplicationTests {
 
     @Test
     public void contextLoads() {
     }
+
 }
