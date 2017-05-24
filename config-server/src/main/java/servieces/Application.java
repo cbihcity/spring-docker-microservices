6be6bcc5d8f734fb75deffb3dfb5190342dd74b2
ihhe0417 <xanderakk@mail.ru>
2017-05-24 11:58:33 +0300

    commit

--- config-server/src/main/java/servieces/Application.java
+++ config-server/src/main/java/servieces/Application.java
@@ -1,13 +1,29 @@
 package servieces;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    @Value("${service.name}")
+    String service;
+
+    @RequestMapping("/")
+    @Bean
+    public String hello(){
+        return "Hello! I'm " + service;
     }
 }
--- gateway/src/main/java/hello/GatewayApplication.java
+++ gateway/src/main/java/hello/GatewayApplication.java
@@ -1,15 +1,20 @@
 package hello;
 
 import hello.filters.pre.SimpleFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
+@RestController
 public class GatewayApplication {
 
   public static void main(String[] args) {
@@ -21,4 +26,13 @@ public class GatewayApplication {
     return new SimpleFilter();
   }
 
+  @Autowired
+  @Value("${service.name}")
+  String service;
+
+  @RequestMapping("/")
+  @Bean
+  public String hello(){
+    return "Hello! I'm " + service;
+  }
 }
